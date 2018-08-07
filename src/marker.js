be46b1a0c1f457d96ad1c6f71781ceb28dd4d589
Daniel Casasola <casasolad86@gmail.com>
2018-08-07 15:42:54 -0500

    buildMarker function

---
+++ src/marker.js
@@ -0,0 +1,21 @@
+
+const mapbox = require('mapbox-gl')
+
+const iconURLs = {
+  hotels: "http://i.imgur.com/D9574Cu.png",
+  restaurants: "http://i.imgur.com/cqR6pUI.png",
+  activities: "http://i.imgur.com/WbMOfMl.png"
+};
+
+
+
+let buildMarker = function (type, cordinates){
+
+  const markerDOMEl = document.createElement('div');
+  markerDOMEl.style.width = "32px";
+  markerDOMEl.style.height = "32px";
+
+
+}
+
+module.exports = buildMarker;
